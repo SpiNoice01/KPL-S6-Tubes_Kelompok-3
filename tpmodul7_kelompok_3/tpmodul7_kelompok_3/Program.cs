43f65cc00b9d62f267f0ef12b03eacceb55be08d
SpiNoice01 <fadhilaagil72@gmail.com>
2025-03-15 22:45:11 +0700

    menambahkan class bacaIsiJson
    ⚠️ Sebelum ini jadi.
    1. Download Newtonsoft.Json. Didalam VS>Tools>Nuget Console dan "dotnet add package Newtonsoft.Json"
    2. Kemudian File json nya di "tpmodul7_kelompok_3\tpmodul7_kelompok_3\bin\Debug\net8.0"
    3. Coding
    4. Run

--- tpmodul7_kelompok_3/tpmodul7_kelompok_3/Program.cs
+++ tpmodul7_kelompok_3/tpmodul7_kelompok_3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class DataMahasiswa<NIM_PRAKTIKAN>
+{
+    public string Nama { get; set; }
+    public string NIM { get; set; }
+    public string Fakultas { get; set; }
+
+    public static void ReadJSON()
+    {
+        string filePath = "tp7_1_<NIM_PRAKTIKAN>.json"; // Ganti dengan nama file yang sudah disimpan
+        if (File.Exists(filePath))
+        {
+            string jsonContent = File.ReadAllText(filePath);
+            var mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa<NIM_PRAKTIKAN>>(jsonContent);
+
+            Console.WriteLine($"Nama {mahasiswa.Nama} dengan NIM {mahasiswa.NIM} dari fakultas {mahasiswa.Fakultas}");
+        }
+        else
+        {
+            Console.WriteLine("File JSON tidak ditemukan!");
+        }
+    }
+}
